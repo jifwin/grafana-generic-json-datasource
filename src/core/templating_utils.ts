3c133f19c408b495e8ba5711b5bc68bd2c347cae
jifwin <>
2018-07-06 19:13:46 +0200

    advanced unpacking

--- src/core/templating_utils.ts
+++ src/core/templating_utils.ts
@@ -12,7 +12,7 @@ export class TemplatingUtils {
         this.scopedVars = scopedVars;
     }
 
-    public replace(expression: string): string[] {
+    public replaceCartesian(expression: string): string[] {
         const replacedExpression = this.templateSrv.replace(expression, this.scopedVars);
         const matchedMultiValues = replacedExpression.match(TemplatingUtils.MULTI_VALUE_REGEX);
 
@@ -32,7 +32,63 @@ export class TemplatingUtils {
         return [replacedExpression];
     }
 
-    public replaceAll(expressions: string[]): string[] {
-        return _.flatten(expressions.map((expression) => this.replace(expression)));
+    public replaceFlat(expression: string) {
+        const replacedExpression = this.templateSrv.replace(expression, this.scopedVars);
+        const matchedMultiValues = _.uniq(replacedExpression.match(TemplatingUtils.MULTI_VALUE_REGEX));
+
+        if (!_.isNil(matchedMultiValues)) {
+            let replacedValues = [replacedExpression];
+            matchedMultiValues.forEach((multiValue) => {
+                const values = multiValue.replace(TemplatingUtils.MULTI_VALUE_BOUNDARIES, "")
+                    .split(TemplatingUtils.MULTI_VALUE_SEPARATOR);
+                replacedValues = _.flatMap(values, (value) => {
+                    return replacedValues.map((replacedValue) => {
+                        return replacedValue.replace(new RegExp(multiValue, "g"), value);
+                    });
+                });
+            });
+            return replacedValues;
+        }
+        return [replacedExpression];
+    }
+
+    public replaceObjectFlat(object: any, properties: string[]) {
+
+        const replacedObject = _.cloneDeep(object);
+        properties.forEach((property) => {
+            replacedObject[property] = this.templateSrv.replace(object[property], this.scopedVars);
+        });
+
+        const matchedMultiValues = _.chain(properties)
+            .map((property) => replacedObject[property])
+            .map((value) => {
+                const matched = value.match(TemplatingUtils.MULTI_VALUE_REGEX);
+                if (_.isArray(matched)) {
+                    return matched[0];
+                }
+                return matched;
+            })
+            .filter((value) => {
+                return !_.isNil(value);
+            })
+            .uniq()
+            .value();
+
+        let replacedObjects = [replacedObject];
+        matchedMultiValues.forEach((multiValue) => {
+            const values = multiValue.replace(TemplatingUtils.MULTI_VALUE_BOUNDARIES, "")
+                .split(TemplatingUtils.MULTI_VALUE_SEPARATOR);
+            replacedObjects = _.flatMap(values, (value) => {
+                return replacedObjects.map((currentReplacedObject) => {
+                    const replacedObjectCopy = _.cloneDeep(currentReplacedObject);
+                    properties.forEach((currentProperty) => {
+                        replacedObjectCopy[currentProperty] = currentReplacedObject[currentProperty].replace(multiValue, value);
+                    });
+                    return replacedObjectCopy;
+                });
+            });
+        });
+
+        return replacedObjects;
     }
 }
